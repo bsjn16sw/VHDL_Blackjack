e69bd92a9f84ac9162abe4557d8e6cdb91d23a7a
jjayd <jjayd1@naver.com>
2019-06-06 12:30:06 +0900

    sec고려

--- random.vhd
+++ random.vhd
@@ -4,6 +4,7 @@ use IEEE.math_real.all;
 
 entity player is
 	port(
+	rst : in std_logic;
 	clk : in std_logic;
 	start : in std_logic;
 	load_stay : in std_logic;
@@ -19,7 +20,8 @@ entity player is
 	tmp8 : out integer;
 	tmp9 : out integer;
 	total : out integer;
-	fin : out std_logic
+	fin : out std_logic;
+	limit : out std_logic
 	);
 end player;
 
@@ -33,14 +35,18 @@ signal en : std_logic:='0';
 type int_arr is array (0 to 15) of integer;
 signal card : int_arr;
 signal term : std_logic := '0';
+signal tterm : std_logic := '0';
+
 signal cardsum: integer := 0;
 signal tmpstart : std_logic;
-signal ttmpstart : std_logic :='1'; --せせせせせせせせせ 益 拭君亜 蟹澗 戚政亜 製 鎧噛杷屡稽澗 in port稽 閤精杏 tmpstart拭 隔醸摂焼 益杏 公壱帖澗暗旭焼!
-												-- 訊劃馬檎 start澗 牌雌 '1'戚壱 益惟 牌雌 tmpstart拭 級嬢亜壱 赤澗汽 購拭 嬢恐 繕闇庚拭辞 tmpstart<='0'馬檎 照鞠澗 汗界旋汗界^^
-												-- 戚訓井酔拭澗 痕呪 煽魚姥稽 馬蟹希 識情馬壱 tmpstart庚税 繕闇庚拭辞 ttmpstart研 闇球形辞 益杏稽 繕箭背醤馬澗牛ぞ,,,
+signal ttmpstart : std_logic :='1';
+signal s1_clk, s2_clk, s14_clk: STD_LOGIC;
+signal my_limit: std_logic := '0';
+
 begin
 
 	tmpstart <= start;
+	limit <= my_limit;
 	
 	process(clk)
 	begin
@@ -55,7 +61,7 @@ begin
 	--		if idx < 10 then
 			if tmpstart='1' and idx < 15 and ttmpstart='1' then -- 昔畿什 岨 郊峨澗汽, 床傾奄葵 格巷 弦戚 級嬢亜辞 袷 企中 3~11稽 照穿廃 葵生稽 郊嘩ぞ
 				en <= '1';
-				b <= a mod 10 +1; -- せ せ せ せ せ せ せ せ b葵聖 旭精 昔銅閃?拭 隔嬢爽壱 宜軒艦猿 暁 喫せ せ せ せ
+				b <= a mod 16 +1; -- せ せ せ せ せ せ せ せ b葵聖 旭精 昔銅閃?拭 隔嬢爽壱 宜軒艦猿 暁 喫せ せ せ せ
 				tmp <= b;
 				card(idx) <= tmp;
 				idx<=idx+1;
@@ -69,13 +75,13 @@ begin
 			end if;
 	---------------------------------------------------	
 		--	report "term value is" & std_logic'image(term); -- 紬繋獄什 重企建 降胃厭,,,,,,,,,,,,,,,,,ぞ,,,,,,,中維,,,,,,, tb宜険凶 嬬車但拭彊,,ぞ
-			if term = '1' then
+			if term = '1' and tterm = '0' then
 				-- display " stay or hit? "
 				-- display card? 
 				if load_stay = '0' then -- stay
 					-- display stay					
 					fin <= '1';	-- turn to next player
-					ttmpstart <= '0';
+			--		ttmpstart <= '0';
 					term <= '0';
 
 				elsif load_hit = '0' then -- hit
@@ -87,39 +93,60 @@ begin
 						-- display burst
 						term <= '0';
 						fin <= '1';
-						ttmpstart <= '0';
+			--			ttmpstart <= '0';
 					end if;
 				end if;
+	
 			end if;
 		end if;
 	end process;
 	
---	process(clk)
---	begin
---		if rising_edge(clk) then
---			if term = '1' then
-				-- display " stay or hit? "
-				-- display card? 
---				if load_stay = '0' then -- stay
-					-- display stay					
---					fin <= '1';	-- turn to next player
---					tmpstart <= '0';
---					term <= '0';
-
---				elsif load_hit = '0' then -- hit
-					-- display hit
---					cardsum <= cardsum + card(idx);
---					idx <= idx + 1;
---					if cardsum>21 then	-- burst
-						-- display burst
---						term <= '0';
---						fin <= '1';
---						tmpstart <= '0';
---					end if;
---				end if;
---			end if;
---		end if;
---	end process;
+	process(s2_clk) -- 15段引
+	begin
+		if rising_edge(s2_clk) then
+			if load_stay='1' and load_hit='1' and term='1' then
+				my_limit <= '1';
+				tterm <= '1';
+				fin <= '1';
+			end if;
+		end if;	
+	end process;
+	
+	-- 1. Clock generator (Period = 1s)
+	process(rst, clk)
+		variable s1_cnt: integer range 0 to 2000000;
+	begin
+		if rst = '0' then
+			s1_clk <= '1';
+			s1_cnt := 0;
+		elsif clk'event and clk = '1' then
+			if s1_cnt < 2000000 then
+				s1_cnt := s1_cnt + 1;
+			else
+				s1_clk <= s1_clk;
+				s1_cnt := 0;
+			end if;
+		end if;
+	end process;
+	
+	-- 2. Clock generator (Period = 2s, 14s)
+	process(rst, s1_clk)
+		variable s14_cnt: integer range 0 to 7;
+	begin
+		if rst = '0' then
+			s2_clk <= '1';
+			s14_clk <= '1';
+			s14_cnt := 0;
+		elsif s1_clk'event and s1_clk = '1' then
+			s2_clk <= not s2_clk;
+			if s14_cnt < 7 then
+				s14_cnt := s14_cnt + 1;
+			else
+				s14_clk <= not s14_clk;
+				s14_cnt := 0;
+			end if;
+		end if;
+	end process;
 	
 	tmp1 <= card(3);
 	tmp2 <= card(4);
@@ -144,6 +171,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity blackjack is
 	port(
+		rst : in std_logic;
 		clk : in std_logic;
 		start : in std_logic;
 		load_stay : in std_logic;
@@ -159,7 +187,10 @@ entity blackjack is
 		tmp8 : out integer;
 		tmp9 : out integer;
 		total1, total2, total3 : out integer;
-		fin : out std_logic
+		fin : out std_logic;
+		limit1 : out std_logic;
+		limit2 : out std_logic;
+		limit3 : out std_logic
 		);
 end blackjack;
 
@@ -170,12 +201,14 @@ signal fin2: std_logic;
 signal fin3: std_logic;
 signal r1,r2,r3 : integer;
 signal t1,t2,t3 : integer;
+signal limitt1, limitt2, limitt3: std_logic;
 
 type int_arr2 is array (0 to 30) of integer;
 signal ttmp : int_arr2;
 
 component player is
 	port(
+	rst : in std_logic;
 	clk : in std_logic;
 	start : in std_logic;
 	load_stay : in std_logic;
@@ -191,14 +224,15 @@ component player is
 	tmp8 : out integer;
 	tmp9 : out integer;
 	total : out integer;
-	fin : out std_logic
+	fin : out std_logic;
+	limit : out std_logic
 	);
 end component;
 
 begin
-	p1 : player port map(clk, start, load_stay, load_hit,r1,ttmp(0),ttmp(1),ttmp(2),ttmp(3),ttmp(4),ttmp(5),ttmp(6),ttmp(7),ttmp(8),t1,fin1);
-	p2 : player port map(clk, fin1, load_stay, load_hit,r2,ttmp(9),ttmp(10),ttmp(11),ttmp(12),ttmp(13),ttmp(14),ttmp(15),ttmp(16),ttmp(17),t2,fin2);
-	p3 : player port map(clk, fin2,load_stay, load_hit, r3,ttmp(18),ttmp(19),ttmp(20),ttmp(21),ttmp(22),ttmp(23),ttmp(24),ttmp(25),ttmp(26),t3,fin3);
+	p1 : player port map(rst, clk, start, load_stay, load_hit,r1,ttmp(0),ttmp(1),ttmp(2),ttmp(3),ttmp(4),ttmp(5),ttmp(6),ttmp(7),ttmp(8),t1,fin1, limitt1);
+	p2 : player port map(rst, clk, fin1, load_stay, load_hit,r2,ttmp(9),ttmp(10),ttmp(11),ttmp(12),ttmp(13),ttmp(14),ttmp(15),ttmp(16),ttmp(17),t2,fin2, limitt2);
+	p3 : player port map(rst, clk, fin2,load_stay, load_hit, r3,ttmp(18),ttmp(19),ttmp(20),ttmp(21),ttmp(22),ttmp(23),ttmp(24),ttmp(25),ttmp(26),t3,fin3, limitt3);
 	
 	tmp1<=ttmp(0);
 	tmp2<=ttmp(1);
@@ -217,5 +251,9 @@ begin
 	rand<=r3;
 
 	fin <= fin3;
+	
+	limit1 <= limitt1;
+	limit2 <= limitt2;
+	limit3 <= limitt3;
 
 end Behavioral;
